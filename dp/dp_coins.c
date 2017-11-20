f45001ef7217611015acf9a7ed53c8e33cfdd507
Terry Zhao <terry.zhao@keylert.com>
2017-11-20 12:18:23 +0800

    dp_coins finished

--- dp/dp_coins.c
+++ dp/dp_coins.c
@@ -25,4 +25,64 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <limits.h>
+
+const int INF = 1e9 + 10;
+
+
+typedef struct {
+    int nCoin; //使用硬币数量
+    int lastSum;//上一个状态
+    int addCoin;//从上一个状态达到当前状态所用的硬币种类
+} state;
+
+
+int makeChange(int totalMoney, int* coins, int coinNum)
+{
+    int total = totalMoney;
+
+    state* sum = (state *)malloc(sizeof(state)*(total+1));
+
+    //init
+    for(int i=0; i<=total; i++)
+        sum[i].nCoin = INF;
+
+    sum[0].nCoin = 0;
+    sum[0].lastSum = 0;
+
+    for(int i=1; i<=total; i++)
+    {
+        for(int j=0; j<coinNum; j++)
+        {
+            if(i-coins[j] >= 0)
+            {
+                if((sum[i-coins[j]].nCoin+1)<sum[i].nCoin){
+                    sum[i].nCoin = sum[i-coins[j]].nCoin+1;
+                    sum[i].lastSum = j;
+                    sum[i].addCoin = coins[j];
+                }
+            }
+
+        }
+    }
+
+    if(sum[total].nCoin == INF){
+        printf("can not make changes!\n");
+        return -1;
+    }else
+        return sum[total].nCoin;
+}
+
+int main()
+{
+    int totalMoney = 100;
+    int coins[3] = {1, 3, 5};
+
+    int num = makeChange(totalMoney, coins, 3);
+    printf("min number of coins: %d\n", num);
+
+
+    return 0;
+}
 
